d36486b5137ed594ae076607a946a5cad2ba49a9
Aline Milene <alinem2104@gmail.com>
2022-03-21 00:55:49 -0300

    Added comments in class message

--- projeto-udp/udp/Mensagem.java
+++ projeto-udp/udp/Mensagem.java
@@ -1,6 +1,7 @@
+// Classe para implementação do cabeçalho das mensagens da transmissão       
 package udp;
 
-
+// Importação de biblioteca auxiliares
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,10 +9,12 @@ import java.util.stream.Collectors;
 
 public class Mensagem {
 
+    // Declaração de variáveis que serão os atributos para o cabeçalho da mensagem
     private String id;
     private String mensagem;
 
-
+    // Método para converter strings para um Map (Json) 
+    // O retorno desta função é o objeto Map
     public Map<String, String> convertToMap(){
         Map<String, String> dado = new HashMap<>();
         dado.put("id", id);
@@ -19,6 +22,8 @@ public class Mensagem {
         return dado;
     }
 
+    // Método para converter o Map para uma String
+    // O retorno desta função é uma string
     public String convertToString(){
         var dado = convertToMap();
         return dado.keySet().stream()
@@ -26,39 +31,43 @@ public class Mensagem {
                 .collect(Collectors.joining(", ", "{", "}"));
     }
 
-    public void setMesagemPorTexto(String mapAsString){
-
+    // Método que recebe uma string e instancia um Map (Json) com a estrutura do cabeçalho 
+    public void setMensagemPorTexto(String mapAsString){
 
         Map<String, String> map = Arrays.stream(mapAsString.substring(1, mapAsString.length()-1).split(","))
                 .map(entry -> entry.split("="))
                 .collect(Collectors.toMap(entry -> entry[0].trim(), entry -> entry[1]));
 
-
         setId(map.get("id"));
-        setMensagem(map.get("mensagem"));
+        setMensagemPorTexto(map.get("mensagem"));
     }
 
-
+    // Construtor inicial da classe mensagem
     public Mensagem(String id, String msg) {
         this.id = id;
         this.mensagem = msg;
     }
 
+    // Construtor vazio para a classe mensagem
     public Mensagem() {
     }
 
+    // Construtor get do atributo Id 
     public String getId() {
         return id;
     }
 
+    // Construtor set atributo Id
     public void setId(String id) {
         this.id = id;
     }
 
+    // Construtor get para retornar a mensagem
     public String getMensagem() {
         return mensagem;
     }
 
+    // Construtor set incluir a mensagem
     public void setMensagem(String mensagem) {
         this.mensagem = mensagem;
     }
